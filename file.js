2872d874b880580f092f90a5e5687baf4dd5da05
Alex <alexkovalenkovk@gmail.com>
2021-11-18 00:21:13 +0100

    Node commit

--- file.js
+++ file.js
@@ -1,12 +1,30 @@
 const http = require('http');
 const fs = require('fs');
+var path = require('path');
 
-const server = http.createServer((req, res) => { 
-	fs.readFile('./index.html', (err, data) => {
-		res.writeHead(200, {'Content-Type': 'text/html'});   
-		res.write(data);
-		res.end();
-	});
+const server = http.createServer((request, response) => { 
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
+    });
 });
 
 server.listen(3000, () => console.log('Server started'));
